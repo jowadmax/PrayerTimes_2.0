cdac1fe4e70dadc898f30197c2db3abb47c29009
jowadmax <jowadmax205@yahoo.com>
2013-05-09 03:47:40 -0500

    add timeTaken entry to Prayer object, preparing to implement getTimes method

--- src/com/example/PrayerTimes/Prayer.java
+++ src/com/example/PrayerTimes/Prayer.java
@@ -3,6 +3,7 @@ package com.example.PrayerTimes;
 public class Prayer {
 	double desiredAngle;
 	int prayerTime;
+	int timeTaken;
 	String type; // "exact", "max"
 	String name;
 	Prayer(double desAng, String type, String name){
@@ -10,9 +11,5 @@ public class Prayer {
 		this.prayerTime = 0;
 		this.type = type;
 		this.name = name;
-	}
-	
-	public void setTime(int second){
-		this.prayerTime = second;
 	}
 }
