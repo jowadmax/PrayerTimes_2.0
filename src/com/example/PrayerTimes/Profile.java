d593a91fcb79a2af924f49039a7019dd960d787b
jowadmax <jowadmax205@yahoo.com>
2013-05-19 11:15:53 -0500

    Added Parcel support for Profile class to allow exchanging between intents

--- src/com/example/PrayerTimes/Profile.java
+++ src/com/example/PrayerTimes/Profile.java
@@ -1,11 +1,49 @@
 package com.example.PrayerTimes;
 
-public class Profile {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Profile implements Parcelable {
 	double 	savedLatitude,
 	savedLongitude;
 	int		savedTimezone;
 	String	cityName;
 	boolean	useGPS,
 	useTimezone;
+	
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeDouble(savedLatitude);		
+		dest.writeDouble(savedLongitude);
+		dest.writeInt(savedTimezone);
+		dest.writeString(cityName);
+		dest.writeByte((byte) (useGPS ? 1 : 0));
+		dest.writeByte((byte) (useTimezone ? 1 : 0));
+	}
+	public static final Parcelable.Creator<Profile> CREATOR = new Parcelable.Creator<Profile>() {
+        public Profile createFromParcel(Parcel in) {
+            return new Profile(in);
+        }
+
+        public Profile[] newArray(int size) {
+            return new Profile[size];
+        }
+    };
+    private Profile(Parcel in) {
+    	savedLatitude = in.readDouble(); 
+    	savedLongitude = in.readDouble(); 
+    	savedTimezone = in.readInt();
+    	cityName = in.readString();
+    	useGPS = in.readByte() == 1;
+    	useTimezone = in.readByte() == 1;
+    }
+	public Profile() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
