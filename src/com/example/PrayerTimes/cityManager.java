41c27bcc0e596eb037401649403124e8f06da773
jowadmax <jowadmax205@yahoo.com>
2013-05-19 10:07:58 -0500

    Changed the view from Alert dialog to a listView to enable the listView features like OnItemLongClickListener.

--- src/com/example/PrayerTimes/cityManager.java
+++ src/com/example/PrayerTimes/cityManager.java
@@ -3,24 +3,41 @@ package com.example.PrayerTimes;
 import java.util.Vector;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
-public final class cityManager extends Activity implements OnClickListener, OnCancelListener {
+public final class cityManager extends Activity implements OnItemClickListener, OnItemLongClickListener, OnCancelListener {
 
+
+	private ListView cityList;
+	private ArrayAdapter<String> cities;
+	String operationType;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		setContentView(R.layout.city_listview);
+		
 		// Receive the data passed from MainActivity
 		Bundle params = getIntent().getExtras();
-		String operationType = params.getString("operationType");
-
+		operationType = params.getString("operationType");
+		
+		// Set the dialog settings
+		if(operationType.equals("save"))
+			this.setTitle("Choose a city to save to:");
+		else
+			this.setTitle("Choose a city to load from:");		
+		
 		// Construct the cities list
 		Vector<String> items = new Vector<String>();
 		if(operationType.equals("save"))
@@ -31,34 +48,32 @@ public final class cityManager extends Activity implements OnClickListener, OnCa
 		items.add("Evansville");
 		items.add("Arlington");
 		items.add("Baghdad");
-		
-		
-		// Create a blank Dialog Builder
-		AlertDialog.Builder builder = new AlertDialog.Builder(cityManager.this);
-		builder.setOnCancelListener(this);
 
-		// Set the dialog settings
-		if(operationType.equals("save"))
-			builder.setTitle("Choose a city to save to:");
-		else
-			builder.setTitle("Choose a city to load from:");		
-		builder.setCancelable(true);
+		cityList = (ListView) findViewById(R.id.list);
+		cityList.setOnItemClickListener(this);
+		cityList.setOnItemLongClickListener(this);
+        cities = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items.toArray(new String[0]));
+        
+        // By using setAdapter method, you plugged the ListView with adapter
+        cityList.setAdapter(cities);
+        }
 
-		// Assign the list and set the OnClickListener method
-		builder.setItems(items.toArray(new String[0]), this);
-
-		// Show the dialog
-		AlertDialog alert = builder.create();
-		alert.show();
-	}
 
 	@Override
-	public void onClick(DialogInterface dialog, int item) {
-		Log.v("MyActivity", "Hello!" + item);
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Log.v("MyActivity", "Hello!" + ((TextView)view).getText());
 		this.finish();
 	}
+
+
 	@Override
-	public void onCancel(DialogInterface arg0) {
+	public void onCancel(DialogInterface dialog) {
 		this.finish();
+	}
+
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+		return true;
 	}
 }
